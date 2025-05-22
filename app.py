b8bc3b36bac85d73ca4ff164c4ecbdb680b14bde
Martyn Pratt <martyni@users.noreply.github.com>
2025-05-22 10:41:51 +0000

    initial

--- app.py
+++ app.py
@@ -1,32 +1,36 @@
 import pprint
 class  RobotGrid(object):
-    def __init__(self):
+    def __init__(self,x,y):
+        self.x = abs(x)
+        self.y = abs(y)
         pass
     def isSafe(self, x, y):
         sum_of = x * y
 
         sum_of_digits = 0
         for digit in str(sum_of):
-            sum_of_digits += int(digit)
+            sum_of_digits += abs(int(digit))
             print(digit)
         
-        print(f'total for {x}, {y} is {sum_of_digits}')    
-        return "O" if  sum_of_digits < 19  else "X" 
+        print(f'total for {self.x}, {self.y} is {sum_of_digits}')    
+        return True if  sum_of_digits < 19  else False 
 
-    def printGrid(self, x, y):
-        x_array = [["" for i in range(y)] for i in range(x)]
+    def printGrid(self):
+        x_array = [["" for i in range(self.y)] for i in range(self.x)]
         print(x_array)
-        for i in range(x):
-            for j in range(y):
-                x_array[i][j] = self.isSafe(i,j)
+        for i in range(self.x):
+            for j in range(self.y):
+                x_array[i][j] = "0" if self.isSafe(i,j) else "X"
         for row in x_array:
             print(row)        
                 
 
 if __name__ == "__main__":
-    rg = RobotGrid()
+    x = -999
+    y = 35
+    rg = RobotGrid(x,y)
     total = 100
-    rg.printGrid(999,35)
+    rg.printGrid()
     # for x in range(100):
     #     for y in range(100):
     #         print(f"{x}, {y}")
